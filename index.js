899c4f23af3e04cc2306d4895b918d5b380eb111
Kuya JJ <63089307+jasonaladin@users.noreply.github.com>
2022-12-08 13:36:29 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
+const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
+
+const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
+
+function buyCake(){
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+function buyIceCream(){
+    return{
+        type: BUY_ICE_CREAM,
+        info: 'First redux action'
+    }
+}
+
+const initialCakeState = {
+    numOfCakes:10
+}
+const initialIceCreamState = {
+    numOfIceCream:20
+}
+
+const cakeReducer = (state = initialCakeState, action) =>{
+    switch(action.type){
+        case BUY_CAKE: 
+            return{
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        
+        default:{
+            return state
+        }
+    }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action) =>{
+    switch(action.type){
+        case BUY_ICE_CREAM: 
+            return{
+                ...state,
+                numOfIceCream: state.numOfIceCream - 1
+            }
+        
+        default:{
+            return state
+        }
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer,applyMiddleware(logger))
+console.log('Initial state',store.getState())
+const unsubscribe = store.subscribe(()=>{})
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+unsubscribe()
